996b2d983169204bcef8637c49e2e9e7eb93cec0
Hilmy Fhauzan <hilmyfhauzan8@gmail.com>
2025-12-02 01:27:47 +0800

    Adding new category and updating clearMAL

--- MY-ANIME-LIST.js
+++ MY-ANIME-LIST.js
@@ -2,14 +2,14 @@ const Sheet = SpreadsheetApp.getActiveSpreadsheet();
 const shtInputMAL = Sheet.getSheetByName('Input');
 const shtMAL = Sheet.getSheetByName('MY ANIME LIST');
 
-function SaveAnime() {
+function saveAnime() {
   
   const rawData = shtInputMAL.getRange('E5:E41').getValues();
 
   const animeTitle = rawData[0][0]; //E5
   const type = rawData[2][0]; //E7
   const source = rawData[4][0]; //E9
-  const studio = rawData[6][0]; //E11
+  const studios = rawData[6][0]; //E11
   const premiered = rawData[8][0]; //E13
   const releaseDateBegin = rawData[10][0]; //E15
   const releaseDateEnd = rawData[12][0]; //E17
@@ -21,27 +21,26 @@ function SaveAnime() {
   const progress = rawData[24][0]; //E29
   const personalRating = rawData[26][0]; //E31
   const legalIllegal = rawData[28][0]; //E33
-  const platform =rawData[12][0]; //E17
-  const description = rawData[18][0]; //E23
-  const opensong = rawData[20][0]; //E25
-  const endsong = rawData[22][0]; //E27
+  const platform = rawData[30][0]; //E35
+  const description = rawData[32][0]; //E37
+  const opensong = rawData[34][0]; //E39
+  const endsong = rawData[36][0]; //E42
 
   let row = shtMAL.getRange('O3').getValue();
   row += 3;
-  const rangeIsiMAL1 = shtMAL.getRange('B' + row + ':D'+ row);
-  const rangeIsiMAL2 = shtMAL.getRange('F' + row + ':N'+ row);
+  const MALContentRange1 = shtMAL.getRange('B' + row + ':H'+ row);
+  const MALContentRange2 = shtMAL.getRange('J' + row + ':U'+ row);
   
-  rangeIsiMAL1.setValues([[animetitle,rdbegin,rdend]]);
-  rangeIsiMAL2.setValues([[epscount,genre,watchsts,eps,legal,platform,description,opensong,endsong]]);
+  MALContentRange1.setValues([[animeTitle,type, source, studios, premiered, releaseDateBegin, releaseDateEnd]]);
+  MALContentRange2.setValues([[epsCount,durationPerMinute, genre, demographic, watchStatus, progress, personalRating, legalIllegal, platform, description, opensong, endsong]]);
 
   const rangeSorting = shtMAL.getRange('B3:N' + row);
   rangeSorting.sort({column: 2, ascending: true});
 
-  ClearMAL();
+  clearMAL();
 };
 
-function ClearMAL() {
-  let cellsToClear = ['E5', 'E7', 'E9', 'E11', 'E13', 'E15', 'E17', 'E19', 'E21', 'E23', 'E25', 'E27'];
+function clearMAL() {
+  let cellsToClear = ['E5', 'E7', 'E9', 'E11', 'E13', 'E15', 'E17', 'E19', 'E21', 'E23', 'E25', 'E27', 'E29', 'E31', 'E33', 'E35', 'E37', 'E39', 'E41'];
   shtInputMAL.getRangeList(cellsToClear).clearContent();
-  // shtInputMAL.getRange('E17').setValue('Eps. ');
 };
